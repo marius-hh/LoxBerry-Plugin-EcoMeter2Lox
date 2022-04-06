e937783ffae5e9e3b7a39ba8de92feabf492d7bf
Marius H <mail@marius-h.de>
2022-04-06 23:14:15 +0200

    add json output file

--- webfrontend/html/index.php
+++ webfrontend/html/index.php
@@ -1,8 +1,20 @@
-<html>
- <head>
-  <title>PHP-Test</title>
- </head>
- <body>
- <?php echo '<p>Hallo Welt</p>'; ?>
- </body>
-</html>
+<?php
+//Check if the collector is running. If its not running, it will be started.
+include_once "loxberry_system.php";
+require_once "$lbphtmlauthdir/defines.php";
+require_once "$lbphtmlauthdir/inc.php";
+
+LOGDEB("Load config and data...");
+$config = read_json_file(ECOMETER_CONFIG_FILE);
+$data = read_json_file(ECOMETER_DATA_FILE);
+
+//MQTT Transfer if enabled
+if($config->MQTT_TRANSFER and isset($data))
+{
+    mqttpublish($data);
+}
+
+LOGDEB("Generate output...");
+header('Content-Type: application/json; charset=utf-8');
+echo json_encode($data);
+?>
